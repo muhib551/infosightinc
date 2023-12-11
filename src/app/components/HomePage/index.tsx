82ef31480b7ea3c12638229fb7da7726c36820a3
muhib551 <muhib551@gmail.com>
2023-12-11 18:31:45 +0500

    creating homepage

---
+++ src/app/components/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <p>Hello this is home page</p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/app/pages/homepage/page.tsx
@@ -0,0 +1,3 @@
+import HomePage from "@/app/components/HomePage";
+
+export default HomePage;
