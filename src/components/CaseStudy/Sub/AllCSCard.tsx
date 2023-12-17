5ffc9739753970e05ed129cafffef8c20fed7839
muhib551 <muhib551@gmail.com>
2023-12-17 17:01:29 +0500

    some miner changes regarding mobile view

--- src/components/CaseStudy/Sub/AllCSCard.tsx
+++ src/components/CaseStudy/Sub/AllCSCard.tsx
@@ -10,14 +10,14 @@ const AllCSCard = () => {
     <div className="grid lg:grid-cols-4 gap-5">
       <div className="p-5 border border-gray-300 rounded-lg w-max">
         <Image src={walletIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium mt-10">
+        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
           South Georgia Bank
         </h1>
         <p className="text-gray-500 mb-1">Client success story</p>
         <span className="text-[#C94B12] text-xs font-semibold bg-[#FDEEE8] px-2 py-1 rounded">
           Financial
         </span>
-        <div className="mt-10">
+        <div className="lg:mt-10 mt-5">
           <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
             View PDF
           </button>
@@ -28,14 +28,14 @@ const AllCSCard = () => {
       </div>
       <div className="p-5 border border-gray-300 rounded-lg w-max">
         <Image src={walletIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium mt-10">
+        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
           Banco de Brasil
         </h1>
         <p className="text-gray-500 mb-1">Client success story</p>
         <span className="text-[#C94B12] text-xs font-semibold bg-[#FDEEE8] px-2 py-1 rounded">
           Financial
         </span>
-        <div className="mt-10">
+        <div className="lg:mt-10 mt-5">
           <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
             View PDF
           </button>
@@ -46,12 +46,14 @@ const AllCSCard = () => {
       </div>
       <div className="p-5 border border-gray-300 rounded-lg w-max">
         <Image src={walletIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium mt-10">FWCCU</h1>
+        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
+          FWCCU
+        </h1>
         <p className="text-gray-500 mb-1">Client success story</p>
         <span className="text-[#C94B12] text-xs font-semibold bg-[#FDEEE8] px-2 py-1 rounded">
           Financial
         </span>
-        <div className="mt-10">
+        <div className="lg:mt-10 mt-5">
           <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
             View PDF
           </button>
@@ -62,12 +64,14 @@ const AllCSCard = () => {
       </div>
       <div className="p-5 border border-gray-300 rounded-lg w-max">
         <Image src={flashIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium mt-10">Prarieland</h1>
+        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
+          Prarieland
+        </h1>
         <p className="text-gray-500 mb-1">Client success story</p>
         <span className="text-[#274A68] text-xs font-semibold bg-[#E9F0F7] px-2 py-1 rounded">
           Energy & Electric
         </span>
-        <div className="mt-10">
+        <div className="lg:mt-10 mt-5">
           <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
             View PDF
           </button>
@@ -78,12 +82,14 @@ const AllCSCard = () => {
       </div>
       <div className="p-5 border border-gray-300 rounded-lg w-max">
         <Image src={flashIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium mt-10">Prarieland</h1>
+        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
+          Prarieland
+        </h1>
         <p className="text-gray-500 mb-1">Client success story</p>
         <span className="text-[#274A68] text-xs font-semibold bg-[#E9F0F7] px-2 py-1 rounded">
           Energy & Electric
         </span>
-        <div className="mt-10">
+        <div className="lg:mt-10 mt-5">
           <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
             View PDF
           </button>
@@ -94,12 +100,14 @@ const AllCSCard = () => {
       </div>
       <div className="p-5 border border-gray-300 rounded-lg w-max">
         <Image src={dropIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium mt-10">Greensboro</h1>
+        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
+          Greensboro
+        </h1>
         <p className="text-gray-500 mb-1">Client success story</p>
         <span className="text-[#364BBA] text-xs font-semibold bg-[#E9F0F7] px-2 py-1 rounded">
           Water & Wastewater
         </span>
-        <div className="mt-10">
+        <div className="lg:mt-10 mt-5">
           <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
             View PDF
           </button>
@@ -110,14 +118,14 @@ const AllCSCard = () => {
       </div>
       <div className="p-5 border border-gray-300 rounded-lg w-max">
         <Image src={judgeIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium mt-10">
+        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
           Quirch Foods
         </h1>
         <p className="text-gray-500 mb-1">Client success story</p>
         <span className="text-gray-700 text-xs font-semibold bg-[#F2F2F2] px-2 py-1 rounded">
           Manufacturing
         </span>
-        <div className="mt-10">
+        <div className="lg:mt-10 mt-5">
           <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
             View PDF
           </button>
