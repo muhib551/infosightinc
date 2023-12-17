12712bad7f171eea69889564a2a9b99923c30f45
muhib551 <muhib551@gmail.com>
2023-12-17 16:58:07 +0500

    some miner changes regard

--- src/components/CaseStudy/Sub/AllCaseStudies.tsx
+++ src/components/CaseStudy/Sub/AllCaseStudies.tsx
@@ -6,14 +6,18 @@ import AllCSCard from "./AllCSCard";
 const AllCaseStudies = () => {
   return (
     <div className="p-5 lg:px-20">
-      <div className="flex flex-col lg:flex-row justify-between gap-2 mb-8">
-        <h1 className="text-2xl lg:text-4xl font-extrabold ">
+      <div className="flex items-center justify-between gap-2 mb-8">
+        <h1 className="text-xl lg:text-4xl font-extrabold ">
           All Case Studies
         </h1>
-        <button className="flex items-center gap-2 px-5 py-2 w-max rounded-lg shadow-md shadow-gray-300">
+        <button className="flex items-center text-xs lg:text-base gap-2 px-2 lg:px-5 py-2 rounded-md border border-gray-300 shadow-md shadow-gray-300 hover:bg-gray-100">
           Select Industry{" "}
           <span>
-            <Image src={DDIcon} alt="" />
+            <Image
+              src={DDIcon}
+              alt="Dropdown icon"
+              className="lg:w-full w-3/4"
+            />
           </span>
         </button>
       </div>
