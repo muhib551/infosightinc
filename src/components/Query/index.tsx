77dee733f95d000e96f843daa027343314c1725e
muhib551 <muhib551@gmail.com>
2023-12-18 21:45:15 +0500

    created query page

---
+++ src/components/Query/index.tsx
@@ -0,0 +1,130 @@
+import React from "react";
+import NavBar from "../Elements/NavBar";
+import Footer from "../Elements/Footer";
+import Button from "../Elements/Button";
+import ContactInfo from "../ContactUs/Sub/ContactInfo";
+import Image from "next/image";
+import locationIcon from "../../../public/location.svg";
+import callingIcon from "../../../public/call-calling.svg";
+import smsddIcon from "../../../public/sms-dd.svg";
+
+const Query = () => {
+  return (
+    <div>
+      <NavBar />
+      <p className="p-5 lg:px-20">Here will be path</p>
+      <div className="p-5 lg:px-20 flex flex-col gap-5 lg:gap-10 lg:flex-row lg:items-start w-full">
+        {/* <span className="lg:hidden">
+          <ContactInfo />
+        </span> */}
+        <div className="w-full">
+          <h1 className="text-gray-900 text-2xl font-semibold">
+            Send Us Your Query
+          </h1>
+          <form action="" className="flex flex-col gap-6 py-5">
+            <div className="flex flex-col lg:flex-row  gap-6 ">
+              <span className="flex flex-col w-full gap-1">
+                <label htmlFor="" className="text-gray-700 ">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your first name"
+                  className="w-full border border-gray-400 p-3 rounded-lg focus:outline-gray-400"
+                />
+              </span>
+              <span className="flex flex-col w-full gap-1">
+                <label htmlFor="" className="text-gray-700 ">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your last name"
+                  className="w-full border border-gray-400 p-3 rounded-lg focus:outline-gray-400"
+                />
+              </span>
+            </div>
+            <div className="flex flex-col lg:flex-row  gap-6 ">
+              <span className="flex flex-col w-full gap-1">
+                <label htmlFor="" className="text-gray-700 ">
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your phone number"
+                  className="w-full border border-gray-400 p-3 rounded-lg focus:outline-gray-400"
+                />
+              </span>
+              <span className="flex flex-col w-full gap-1">
+                <label htmlFor="" className="text-gray-700 ">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your email address"
+                  className="w-full border border-gray-400 p-3 rounded-lg focus:outline-gray-400"
+                />
+              </span>
+            </div>
+            <div className="flex flex-col lg:flex-row  gap-6 ">
+              <span className="flex flex-col w-full gap-1">
+                <label htmlFor="" className="text-gray-700 ">
+                  Company
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter the compnay name"
+                  className="w-full border border-gray-400 p-3 rounded-lg focus:outline-gray-400"
+                />
+              </span>
+              <span className="w-full"></span>
+            </div>
+            <span className="flex flex-col gap-1">
+              <label htmlFor="" className="text-gray-700 ">
+                Your Message
+              </label>
+              <textarea
+                className="border border-gray-400 p-3 rounded-lg focus:outline-gray-400"
+                name="Messages"
+                placeholder="Type here..."
+                id=""
+                cols={10}
+                rows={4}
+              ></textarea>
+            </span>
+          </form>
+          <div className=" text-end my-5">
+            <Button
+              text="Submit"
+              style={{
+                padding: "10px 30px",
+              }}
+            />
+          </div>
+        </div>
+        <div className="border border-gray-300 p-8 rounded-xl lg:mt-5">
+          <h1 className="text-xl text-gray-900">Our Contact Detail</h1>
+          <div className="flex flex-col gap-5 mt-5">
+            <span className="flex items-center gap-4">
+              <Image src={locationIcon} alt="Location icon" className="" />
+              <p className="text-gray-500">
+                14100, Palmetto Frontage Road, Suite 310, Miami Lakes, FL 33016
+              </p>
+            </span>
+            <span className="flex items-center gap-4">
+              <Image src={callingIcon} alt="Call icon" className="" />
+              <p className="text-gray-500">305.828.1003</p>
+            </span>
+            <span className="flex items-center gap-4">
+              <Image src={smsddIcon} alt="Mail icon" className="" />
+              <p className="text-gray-500">info@infosightinc.com</p>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Query;
