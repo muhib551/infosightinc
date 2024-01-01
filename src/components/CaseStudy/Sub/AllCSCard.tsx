e94c0139f0f12799426694fa8cf33a5b8cec5465
muhib551 <muhib551@gmail.com>
2024-01-01 21:41:29 +0500

    modified some changes

--- src/components/CaseStudy/Sub/AllCSCard.tsx
+++ src/components/CaseStudy/Sub/AllCSCard.tsx
@@ -4,6 +4,7 @@ import walletIcon from "../../../../public/casestudy/walle-icon.svg";
 import flashIcon from "../../../../public/casestudy/flash-icon.svg";
 import dropIcon from "../../../../public/casestudy/drop-icon.svg";
 import judgeIcon from "../../../../public/casestudy/judge-icon.svg";
+import Link from "next/link";
 
 const ALLCARDS_DATA = [
   {
@@ -11,49 +12,56 @@ const ALLCARDS_DATA = [
     title: "South Georgia Bank",
     description: "Client success story",
     tag: "Financial",
+    link: "https://www.infosightinc.com/pdf/InfoSight%20-%20South%20GA%20Bank%20Client%20Success%20Story.pdf",
   },
   {
     img: walletIcon,
     title: "Banco de Brasil",
     description: "Client success story",
     tag: "Financial",
+    link: "https://www.infosightinc.com/pdf/InfoSight%20-%20Banco%20do%20Brasil%20Americas%20Client%20Success%20Story.pdf",
   },
   {
     img: walletIcon,
     title: "FWCCU",
     description: "Client success story",
     tag: "Financial",
+    link: "https://www.infosightinc.com/pdf/InfoSight%20-%20FWCCU%20Client%20Success%20Story.pdf",
   },
   {
     img: flashIcon,
     title: "Prarieland",
     description: "Client success story",
     tag: "Energy & Electric",
+    link: "https://www.infosightinc.com/pdf/InfoSight%20-%20Prairie%20Land%20Electric%20Client%20Success%20Story.pdf",
   },
   {
     img: flashIcon,
-    title: "Prarieland",
+    title: "Pioneer",
     description: "Client success story",
     tag: "Energy & Electric",
+    link: "https://www.infosightinc.com/pdf/InfoSight%20-%20Pioneer%20Electric%20Client%20Success%20Story.pdf",
   },
   {
     img: dropIcon,
     title: "Greensboro",
     description: "Client success story",
     tag: "Water & Wastewater",
+    link: "https://www.infosightinc.com/pdf/InfoSight%20-%20City%20of%20Greensboro%20Water%20Resources%20Client%20Success%20Story.pdf",
   },
   {
     img: judgeIcon,
     title: " Quirch Foods",
     description: "Client success story",
     tag: "Manufacturing",
+    link: "https://www.infosightinc.com/pdf/InfoSight%20-%20Quirch%20Foods%20Client%20Success%20Story.pdf",
   },
 ];
 
 const AllCSCard = () => {
   return (
-    <div className="grid lg:grid-cols-4 gap-5">
-      {/* {ALLCARDS_DATA.map(({ img, title, description, tag }, i) => {
+    <div className="grid items-center lg:grid-cols-4 gap-5">
+      {ALLCARDS_DATA.map(({ img, title, description, tag, link }, i) => {
         return (
           <div
             className="p-5 border border-gray-300 rounded-lg w-max"
@@ -67,144 +75,21 @@ const AllCSCard = () => {
             <span className="text-[#C94B12] text-xs font-semibold bg-[#FDEEE8] px-2 py-1 rounded">
               {tag}
             </span>
-            <div className="lg:mt-10 mt-5">
-              <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
-                View PDF
-              </button>
-              <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md">
-                Download PDF
-              </button>
+            <div className="lg:mt-10 mt-5 flex lg:gap-5 gap-3">
+              <Link href={link} target="_blank">
+                <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md">
+                  View PDF
+                </button>
+              </Link>
+              <Link href={link} target="_blank">
+                <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md ">
+                  Download PDF
+                </button>
+              </Link>
             </div>
           </div>
         );
-      })} */}
-
-      <div className="p-5 border border-gray-300 rounded-lg w-max">
-        <Image src={walletIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
-          South Georgia Bank
-        </h1>
-        <p className="text-gray-500 mb-1">Client success story</p>
-        <span className="text-[#C94B12] text-xs font-semibold bg-[#FDEEE8] px-2 py-1 rounded">
-          Financial
-        </span>
-        <div className="lg:mt-10 mt-5">
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
-            View PDF
-          </button>
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md">
-            Download PDF
-          </button>
-        </div>
-      </div>
-      <div className="p-5 border border-gray-300 rounded-lg w-max">
-        <Image src={walletIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
-          Banco de Brasil
-        </h1>
-        <p className="text-gray-500 mb-1">Client success story</p>
-        <span className="text-[#C94B12] text-xs font-semibold bg-[#FDEEE8] px-2 py-1 rounded">
-          Financial
-        </span>
-        <div className="lg:mt-10 mt-5">
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
-            View PDF
-          </button>
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md">
-            Download PDF
-          </button>
-        </div>
-      </div>
-      <div className="p-5 border border-gray-300 rounded-lg w-max">
-        <Image src={walletIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
-          FWCCU
-        </h1>
-        <p className="text-gray-500 mb-1">Client success story</p>
-        <span className="text-[#C94B12] text-xs font-semibold bg-[#FDEEE8] px-2 py-1 rounded">
-          Financial
-        </span>
-        <div className="lg:mt-10 mt-5">
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
-            View PDF
-          </button>
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md">
-            Download PDF
-          </button>
-        </div>
-      </div>
-      <div className="p-5 border border-gray-300 rounded-lg w-max">
-        <Image src={flashIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
-          Prarieland
-        </h1>
-        <p className="text-gray-500 mb-1">Client success story</p>
-        <span className="text-[#274A68] text-xs font-semibold bg-[#E9F0F7] px-2 py-1 rounded">
-          Energy & Electric
-        </span>
-        <div className="lg:mt-10 mt-5">
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
-            View PDF
-          </button>
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md">
-            Download PDF
-          </button>
-        </div>
-      </div>
-      <div className="p-5 border border-gray-300 rounded-lg w-max">
-        <Image src={flashIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
-          Prarieland
-        </h1>
-        <p className="text-gray-500 mb-1">Client success story</p>
-        <span className="text-[#274A68] text-xs font-semibold bg-[#E9F0F7] px-2 py-1 rounded">
-          Energy & Electric
-        </span>
-        <div className="lg:mt-10 mt-5">
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
-            View PDF
-          </button>
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md">
-            Download PDF
-          </button>
-        </div>
-      </div>
-      <div className="p-5 border border-gray-300 rounded-lg w-max">
-        <Image src={dropIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
-          Greensboro
-        </h1>
-        <p className="text-gray-500 mb-1">Client success story</p>
-        <span className="text-[#364BBA] text-xs font-semibold bg-[#E9F0F7] px-2 py-1 rounded">
-          Water & Wastewater
-        </span>
-        <div className="lg:mt-10 mt-5">
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
-            View PDF
-          </button>
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md">
-            Download PDF
-          </button>
-        </div>
-      </div>
-      <div className="p-5 border border-gray-300 rounded-lg w-max">
-        <Image src={judgeIcon} alt="All Case Studies Icon" className="" />
-        <h1 className="text-xl text-gray-900 font-medium lg:mt-10 mt-5">
-          Quirch Foods
-        </h1>
-        <p className="text-gray-500 mb-1">Client success story</p>
-        <span className="text-gray-700 text-xs font-semibold bg-[#F2F2F2] px-2 py-1 rounded">
-          Manufacturing
-        </span>
-        <div className="lg:mt-10 mt-5">
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md mr-3">
-            View PDF
-          </button>
-          <button className="text-gray-700 border hover:bg-gray-100 border-gray-300 px-5 py-2 rounded-md">
-            Download PDF
-          </button>
-        </div>
-      </div>
+      })}
     </div>
   );
 };
